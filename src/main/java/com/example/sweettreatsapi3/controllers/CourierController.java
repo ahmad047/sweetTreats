f01c99fc8a7974134d84f7d8b0b5d624059640d6
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-10-19 15:48:26 +0100

    added basic controller

---
+++ src/main/java/com/example/sweettreatsapi3/controllers/CourierController.java
@@ -0,0 +1,19 @@
+package com.example.sweettreatsapi3.controllers;
+
+import com.example.sweettreatsapi3.models.Courier;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/couriers")
+public class CourierController {
+//    @GetMapping
+//    List<Courier> all() {
+//    }
+}
