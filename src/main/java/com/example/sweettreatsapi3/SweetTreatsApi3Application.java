fe641b0d50ab9039108cd0430d7f146715323f4f
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-10-19 15:35:23 +0100

    new project

---
+++ src/main/java/com/example/sweettreatsapi3/SweetTreatsApi3Application.java
@@ -0,0 +1,13 @@
+package com.example.sweettreatsapi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetTreatsApi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetTreatsApi3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sweettreatsapi3/SweetTreatsApi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweettreatsapi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetTreatsApi3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
